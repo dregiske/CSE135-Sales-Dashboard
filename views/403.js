5383ae808e0e40a8eb6441e85279bf5e9e6fde1b
Andre Giske <agiske@ucsd.edu>
2026-03-15 16:38:14 -0700

    footer and 403 page

---
+++ views/403.js
@@ -0,0 +1,9 @@
+<%- include('components/header') %>
+<%- include('components/navbar') %>
+<div class="login-container">
+  <h1>403</h1>
+  <h2>Access Denied</h2>
+  <p>You don't have permission to view this page.</p>
+  <a href="/dashboard">Back to Dashboard</a>
+</div>;
+<%- include ('components/footer') %>
