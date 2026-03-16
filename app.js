c843fcf431d49354ae04c07ad9927eeb9a58782c
Andre Giske <agiske@ucsd.edu>
2026-03-15 21:37:47 -0700

    updated app

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
 const app = express();
 const port = process.env.PORT;
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
-app.use(session({
+app.use(express.static("public"));
+app.use(
+  session({
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false }
-}));
+    cookie: { secure: false },
+  }),
+);
 
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+const adminRouter = require("./routes/admin");
 
-app.use('/', indexRouter);
-app.use('/', authRouter);
+app.use("/", indexRouter);
+app.use("/", authRouter);
+app.use("/admin", adminRouter);
 
 app.use((_req, res) => {
-    res.status(404).render('404');
+  res.status(404).render("404");
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
